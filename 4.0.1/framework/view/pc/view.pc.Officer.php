8ca6d4f7435558aada7e03357e34c3e5f0a8d6ff
James Thomas <jthomas@merit.com.au>
2014-08-26 16:37:22 +1000

    Updated Officer View

--- 4.0.1/framework/view/pc/view.pc.Officer.php
+++ 4.0.1/framework/view/pc/view.pc.Officer.php
@@ -4,7 +4,7 @@ $GLOBALS['result'] = $GLOBALS['result']->officer_det;
 
 
 <div class="summaryContainer">
-    <h1>Officer Details</h1>
+    <h1>Details</h1>
 
     <div class="float-left">
         <div class="column r50">
@@ -23,27 +23,15 @@ $GLOBALS['result'] = $GLOBALS['result']->officer_det;
                 </div>
             </div>
 
-            <div class="column r25"> 
-                <span class="summaryColumnTitle">Officer Title</span>
-                <div class="summaryColumn">
-                   <?php if(isset($GLOBALS['result']->class_name)) echo $GLOBALS['result']->class_name; ?>
-                </div>
-            </div>
 
       </div>
    </div>
 </div>
 
 <div class="summaryContainer">
-    <h1>Organization</h1>
+    <h1>Organisation</h1>
         <div class="float-left">
            <div class="column r50">
-            <div class="column r50">
-                <span class="summaryColumnTitle">Location</span>
-                <div class="summaryColumn">
-                    <?php if(isset($GLOBALS['result']->location)) echo $GLOBALS['result']->location; ?>
-                </div>
-            </div>
 
             <div class="column r50"> 
                 <span class="summaryColumnTitle">Division</span>
@@ -52,46 +40,54 @@ $GLOBALS['result'] = $GLOBALS['result']->officer_det;
                 </div>
             </div>
 
-      </div>    
-  </div>
-
-
-  <div class="float-left">
-     <div class="column r50">
-  <div class="column r50"> 
+            <div class="column r50"> 
                 <span class="summaryColumnTitle">Department</span>
                 <div class="summaryColumn">
                     <?php if(isset($GLOBALS['result']->dep_name)) echo $GLOBALS['result']->dep_name; ?>
                 </div>
-          </div>
-          <div class="column r50"> 
+             </div>
+
+             <div class="column r50"> 
                 <span class="summaryColumnTitle">Centre</span>
                 <div class="summaryColumn">
                     <?php if(isset($GLOBALS['result']->centre_name)) echo $GLOBALS['result']->centre_name; ?>
                 </div>
-          </div>
-          </div>
-          
-          </div>
-  <div class="float-left">
-  <div class="column r50"> 
-      <div class="column r25">
-            <span class="summaryColumnTitle">Telephone</span>
-            <div class="summaryColumn"><?php if(isset($GLOBALS['result']->telephone)) echo $GLOBALS['result']->telephone; ?></div>
-       </div>
-      <div class="column r25">
-            <span class="summaryColumnTitle">Mobile No</span>
-            <div class="summaryColumn"><?php if(isset($GLOBALS['result']->mobile_no)) echo $GLOBALS['result']->mobile_no; ?></div>
-      </div>
-      <div class="column r25">
-            <span class="summaryColumnTitle">FAX No</span>
-            <div class="summaryColumn"><?php if(isset($GLOBALS['result']->fax_no)) echo $GLOBALS['result']->fax_no; ?></div>
-        </div>
-        </div>
-  </div>
-  <div class="column r25"> 
-            <span class="summaryColumnTitle">Email Address</span>
-            <div class="summaryColumn">
-                <?php if(isset($GLOBALS['result']->mail_id)) echo $GLOBALS['result']->mail_id; ?>
+            </div>
+
+            <div class="column r50">
+                <span class="summaryColumnTitle">Location</span>
+                <div class="summaryColumn">
+                    <?php if(isset($GLOBALS['result']->location)) echo $GLOBALS['result']->location; ?>
+                </div>
+            </div>   
+      </div>    
+    </div>
+
+
+  <div class="summaryContainer">
+     <h1>Contact</h1>
+        <div class="float-left">
+           <div class="column r50"> 
+      
+            <div class="column r25">
+                   <span class="summaryColumnTitle">Telephone</span>
+                   <div class="summaryColumn"><?php if(isset($GLOBALS['result']->telephone)) echo $GLOBALS['result']->telephone; ?></div>
+            </div>
+
+            <div class="column r25">
+                   <span class="summaryColumnTitle">Mobile No</span>
+                   <div class="summaryColumn"><?php if(isset($GLOBALS['result']->mobile_no)) echo $GLOBALS['result']->mobile_no; ?></div>
+            </div>
+            <div class="column r20">
+                   <span class="summaryColumnTitle">FAX No</span>
+                   <div class="summaryColumn"><?php if(isset($GLOBALS['result']->fax_no)) echo $GLOBALS['result']->fax_no; ?></div>
+            </div>
+            <div class="column r50"> 
+                   <span class="summaryColumnTitle">Email Address</span>
+                   <div class="summaryColumn">
+                   <?php if(isset($GLOBALS['result']->mail_id)) echo $GLOBALS['result']->mail_id; ?>
+                   </div>
             </div>
       </div>
+   </div>
+</div>
