1d19aa377bbbe3571c94fa1eb518cd131dc0e1b3
James Thomas <jthomas@merit.com.au>
2014-08-27 11:43:24 +1000

    Officer Dynamic Page links

--- 4.0.1/framework/view/pc/sidebar/view.sidebar.viewOfficer.php
+++ 4.0.1/framework/view/pc/sidebar/view.sidebar.viewOfficer.php
@@ -1,5 +1,6 @@
+<?php if(isset($_GET['ex'])){ $ex = strip_tags($_GET['ex']); } else { $ex = ""; } ?>
 <ul class="subButtons">
       <li onclick="self.location.href='index.php?page=view-officer&id=<?php echo $_GET['id']; ?>&d=summary'" <?php if(isset($_GET['d']) && $_GET['d'] == "summary" || !isset($_GET['d'])){ ?>class="act"<?php } ?>>Summary</li>
-      <li onclick="self.location.href='index.php?page=view-officer&id=<?php echo $_GET['id']; ?>&d=act'" <?php if(isset($_GET['d']) && $_GET['d'] == "act"){ ?>class="act"<?php } ?>>Actions</li>
-      <li onclick="self.location.href='index.php?page=view-officer&id=<?php echo $_GET['id']; ?>&d=req'" <?php if(isset($_GET['d']) && $_GET['d'] == "req"){ ?>class="act"<?php } ?>>Requests</li>
-  </ul>
+      <li onclick="self.location.href='index.php?page=view-officer&id=<?php echo $_GET['id']; ?>&d=act'" <?php if(isset($_GET['d']) && $_GET['d'] == "act"){ ?>class="act"<?php } ?>>Actions (<?php echo $GLOBALS['actionCount']; ?>)</li>
+      <li onclick="self.location.href='index.php?page=view-officer&id=<?php echo $_GET['id']; ?>&d=req'" <?php if(isset($_GET['d']) && $_GET['d'] == "req"){ ?>class="req"<?php } ?>>Requests  (<?php echo $GLOBALS['requestCount']; ?>)</li>
+</ul>
