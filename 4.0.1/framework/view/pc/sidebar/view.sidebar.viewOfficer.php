3645e6924ed4e22fce8518be00894542c8d337aa
James Thomas <jthomas@merit.com.au>
2014-08-27 12:29:18 +1000

    Officer Dynamic Page Links
    Displays correctly, however still needs changes to the "Action" number

--- 4.0.1/framework/view/pc/sidebar/view.sidebar.viewOfficer.php
+++ 4.0.1/framework/view/pc/sidebar/view.sidebar.viewOfficer.php
@@ -1,6 +1,7 @@
-<?php if(isset($_GET['ex'])){ $ex = strip_tags($_GET['ex']); } else { $ex = ""; } ?>
+
+
 <ul class="subButtons">
       <li onclick="self.location.href='index.php?page=view-officer&id=<?php echo $_GET['id']; ?>&d=summary'" <?php if(isset($_GET['d']) && $_GET['d'] == "summary" || !isset($_GET['d'])){ ?>class="act"<?php } ?>>Summary</li>
       <li onclick="self.location.href='index.php?page=view-officer&id=<?php echo $_GET['id']; ?>&d=act'" <?php if(isset($_GET['d']) && $_GET['d'] == "act"){ ?>class="act"<?php } ?>>Actions (<?php echo $GLOBALS['actionCount']; ?>)</li>
-      <li onclick="self.location.href='index.php?page=view-officer&id=<?php echo $_GET['id']; ?>&d=req'" <?php if(isset($_GET['d']) && $_GET['d'] == "req"){ ?>class="req"<?php } ?>>Requests  (<?php echo $GLOBALS['requestCount']; ?>)</li>
+      <li onclick="self.location.href='index.php?page=view-officer&id=<?php echo $_GET['id']; ?>&d=req'" <?php if(isset($_GET['d']) && $_GET['d'] == "requests"){ ?>class="req"<?php } ?>>Requests  (<?php echo $GLOBALS['requestCount']; ?>)</li>
 </ul>
--- 4.0.1/framework/view/pc/view.pc.OfficerHeader.php
+++ 4.0.1/framework/view/pc/view.pc.OfficerHeader.php
@@ -1,3 +1,8 @@
 <h2>
-    <b><?php if(isset($GLOBALS['result']->officer_det->given_names)) echo $GLOBALS['result']->officer_det->given_names; ?> <?php if(isset($GLOBALS['result']->officer_det->surname)) echo $GLOBALS['result']->officer_det->surname; ?></b>
+    <b><?php 
+    if(isset($GLOBALS['result']->officer_det->given_names)) echo $GLOBALS['result']->officer_det->given_names; ?> <?php if(isset($GLOBALS['result']->officer_det->surname)) echo $GLOBALS['result']->officer_det->surname; ?></b>
  </h2>
+<?php
+$GLOBALS['requestCount'] = count($GLOBALS['result']->req_dets->request_details);
+$GLOBALS['actionCount'] = count($GLOBALS['result']->action_det->action_details);
+?>
