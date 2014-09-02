7c9e27f7f9b2025242c55d642b23428633e4f61c
James Thomas <jthomas@merit.com.au>
2014-09-02 12:21:27 +1000

    Fixed Bugs

--- 4.0.1/framework/view/pc/sidebar/view.sidebar.viewOfficer.php
+++ 4.0.1/framework/view/pc/sidebar/view.sidebar.viewOfficer.php
@@ -1,6 +1,6 @@
 
 <ul class="subButtons">
       <li onclick="self.location.href='index.php?page=view-officer&id=<?php echo $_GET['id'];?>&d=summary'"<?php if(isset($_GET['d']) && $_GET['d'] == "summary" || !isset($_GET['d'])) ?>>Summary</li>
-      <?php if($GLOBALS['actionCount'] > 0){ ?><li onclick="self.location.href='index.php?page=view-officer&id=<?php echo $_GET['id']; ?>&ex=<?php echo $ex; ?>&d=act'" <?php if(isset($_GET['d']) && $_GET['d'] == "requests"){ ?>class="act"<?php } ?>>Requests (<?php echo $GLOBALS['actionCount']; ?>)</li><?php } ?>
-      <?php if($GLOBALS['requestCount'] > 0){ ?><li onclick="self.location.href='index.php?page=view-officer&id=<?php echo $_GET['id']; ?>&ex=<?php echo $ex; ?>&d=req'" <?php if(isset($_GET['d']) && $_GET['d'] == "addresses"){ ?>class="act"<?php } ?>>Addresses (<?php echo $GLOBALS['requestCount']; ?>)</li><?php } ?>
+      <?php if($GLOBALS['actionCount'] > 0){ ?><li onclick="self.location.href='index.php?page=view-officer&id=<?php echo $_GET['id']; ?>&ex=<?php echo $ex; ?>&d=act'" <?php if(isset($_GET['d']) && $_GET['d'] == "requests"){ ?>class="act"<?php } ?>>Actions (<?php echo $GLOBALS['actionCount']; ?>)</li><?php } ?>
+      <?php if($GLOBALS['requestCount'] > 0){ ?><li onclick="self.location.href='index.php?page=view-officer&id=<?php echo $_GET['id']; ?>&ex=<?php echo $ex; ?>&d=req'" <?php if(isset($_GET['d']) && $_GET['d'] == "addresses"){ ?>class="act"<?php } ?>>Requests (<?php echo $GLOBALS['requestCount']; ?>)</li><?php } ?>
 </ul>
