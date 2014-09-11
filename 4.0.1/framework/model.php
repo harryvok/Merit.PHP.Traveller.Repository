56333a1feed450fdada33aec12c740298ca4dbdd
harryvok <harry.v1@hotmail.com>
2014-09-11 10:55:16 +1000

    before changing processNewRequestAttachment

--- 4.0.1/framework/model.php
+++ 4.0.1/framework/model.php
@@ -2112,6 +2112,8 @@ class Model {
     
     public function processnewRequestAttachment($attachment, $requestID, $description = '',$rand){
         
+        
+        
         $max_upload = (int)(ini_get('upload_max_filesize'));
         $max_post = (int)(ini_get('post_max_size'));
         $memory_limit = (int)(ini_get('memory_limit'));
