5c2688374f9e627db2e782571a18f2d82d15281b
James Thomas <jthomas@merit.com.au>
2015-01-07 11:59:13 +1100

    #177 Resizing

--- 4.0.1/page.newRequest.php
+++ 4.0.1/page.newRequest.php
@@ -359,7 +359,7 @@ if(!isset($_GET['d'])){
                             <div class="column r50">
                                 <label for="same">Customer Resides</label>
                                 <br/>
-                                <select class="text" name="same" id="same" onchange="changeLocationType();" style="width:200px">
+                                <select class="text" name="same" id="same" onchange="changeLocationType();" style="width:162px">
 
                                     <?php
                                 if(isset($_SESSION['rem_same'])){
@@ -391,7 +391,7 @@ if(!isset($_GET['d'])){
                                  } ?>
                                 </select>
 
-                                <input type="button" value="Clear" onclick="clearCustomerAddress()" />
+                                <input type="button" value="Clear" onclick="clearCustomerAddress()" / style="margin-left:10px">
                                 <input type="button" value="Summary" disabled id="CustAddSummary" onclick="ViewCustomerAddDetails()"  />
                             </div>
                         </div>
@@ -402,8 +402,8 @@ if(!isset($_GET['d'])){
 		                            <label for="i_cfno">Flat/Unit Number</label>
 		                            <input class="text cadd" name='i_cfno' onChange="" id="i_cfno"maxlength='15' value='<?php if(isset($_SESSION['rem_i_cno'])){ echo $_SESSION['rem_i_cno']; } ?>'>
 		                        </div>
-		                        <div class="column r15">
-		                            <label for="i_cfcode">Flat Suffix</label>
+		                        <div class="column r10">
+		                            <label for="i_cfcode">Unit Suffix</label>
 		                            <input class="text cadd" name='i_cfcode' onChange="" id="i_cfcode"maxlength='15' value='<?php if(isset($_SESSION['rem_i_cno'])){ echo $_SESSION['rem_i_cno']; } ?>'>
 		                        </div>
 
@@ -411,41 +411,41 @@ if(!isset($_GET['d'])){
 		                            <label for="i_cno">Street Number</label>
 		                            <input class="text cadd" name='i_cno' onChange="" id="i_cno"  maxlength='15' value='<?php if(isset($_SESSION['rem_i_cfaddno'])){ echo $_SESSION['rem_i_cfaddno']; } ?>'>
 		                        </div>
-		                        <div class="column r15">
+		                        <div class="column r10">
 		                            <label for="i_cscode">Street Suffix</label>
 		                            <input class="text cadd" name='i_cscode' onChange="" id="i_cscode"  maxlength='15' value='<?php if(isset($_SESSION['rem_i_cfaddno'])){ echo $_SESSION['rem_i_cfaddno']; } ?>'>
 		                        </div>
-
-		                        <div class="column r25">
+                                <div class="float-left">
+		                        <div class="column r20">
 		                            <label for="i_cstreet">Street Name<span class="customer_address_label mandLabel" style="color: red; display:none;"> *</span></label>
 		                            <input class="text cadd checkNone" name='i_cstreet'  id="i_cstreet" data-mand="customer_address"  maxlength='100' value='<?php if(isset($_SESSION['rem_i_cstreet'])){ echo $_SESSION['rem_i_cstreet']; } ?>'>
 		                            <input type="hidden" name="comparei_cstreet" id="comparei_cstreet" />
 		                        </div>
-		                        <div class="column r25">
+
+		                        <div class="column r20">
 		                            <label for="i_ctype">Street Type<span class="customer_address_label mandLabel" style="color: red; display:none;"> *</span></label>
 		                            <input class="text cadd checkNone" name='i_ctype'  id="i_ctype" data-mand="customer_address" disabled="disabled" maxlength='100' value='<?php if(isset($_SESSION['rem_i_ctype'])){ echo $_SESSION['rem_i_ctype']; } ?>'>
 		                            <input type="hidden" name="comparei_ctype" id="comparei_ctype" />
 		                        </div>
-
-		    
-	                            <div class="column r25">
+	    
+	                            <div class="column r20">
 	                                <label for="i_csuburb">Suburb<span class="customer_address_label mandLabel" style="color: red; display:none;"> *</span></label>
 	                                <input class="text cadd checkNone" name='i_csuburb' onchange="" id="i_csuburb" data-mand="customer_address" disabled="disabled" maxlength='100' value='<?php if(isset($_SESSION['rem_i_csuburb'])){ echo $_SESSION['rem_i_csuburb']; }  ?>'>
 	                                <input type="hidden" name="comparei_csuburb" id="comparei_csuburb" />
 	                            </div>
-	                            <div class="column r25">
+	                            <div class="column r15">
 	                                <label for="i_cpostcode">Postcode</label>
 	                                <input class="text cadd" name='i_cpostcode'  id="i_cpostcode" maxlength='6'>
 	                                <input type="hidden" name="comparei_cpostcode" id="comparei_cpostcode" />
 	                            </div>
-
+                                <div class="column r15">
+		                            <label for="i_cpropertynumber">Property Number</label>
+		                            <input class="text cadd" name='i_cpropertynumber'  id="i_cpropertynumber" >
+		                            <input type="hidden" name="comparei_cpropertynumber" id="comparei_cpropertynumber" />
+		                         </div>
+                                    </div>
 		                        <div class="float-left">
-		                            <div class="column r25">
-		                                <label for="i_cpropertynumber">Property Number</label>
-		                                <input class="text cadd" name='i_cpropertynumber'  id="i_cpropertynumber" >
-		                                <input type="hidden" name="comparei_cpropertynumber" id="comparei_cpropertynumber" />
-		                            </div>
-		                            <div class="column r100">
+		                            <div class="column r90">
 		                                <label for="i_cdesc">Description</label>
 		                                <textarea id="i_cdesc" name="i_cdesc" style="resize:none; height:4em" maxlength='1000' value='<?php if(isset($_SESSION['rem_i_cdesc'])){ echo $_SESSION['rem_i_cdesc']; } ?>'></textarea>
 		                            </div>
