7cd0ccc865417f496d688cdbf2ade14eefd779d9
phirpara@merit.com.au <phirpara@merit.com.au>
2015-02-05 14:13:49 +1100

    tutyu

--- 4.0.1/page.view-address.php
+++ 4.0.1/page.view-address.php
@@ -9,6 +9,18 @@
 
 <?php
 $address_id = strip_tags($_GET['id']);
+include_once("model.php");  
+$model = new Model();  
+$parameters = new stdClass();
+$parameters->user_id = $_SESSION['user_id'];
+$parameters->password = $_SESSION['password'];
+$parameters->service_code = "";
+$parameters->request_code = "";
+$parameters->function_code = "";
+$parameters->address_id = $address_id;
+$parameters->property_no = "";  
+$result = $model->WebService(MERIT_REQUEST_FILE, "ws_get_annual_allowance", $parameters);
+$_SESSION["allowance_count"] = count($GLOBALS['result']->allowance_history->annual_allowance_history);
 
 if(isset($_GET['ex'])){ $ex = strip_tags($_GET['ex']); }
 if(isset($_GET['ref'])){ $ref = strip_tags($_GET['ref']); }
@@ -20,6 +32,9 @@ if(isset($_GET['ref_page'])){ $page = strip_tags($_GET['ref_page']); }
  }
  if(isset($_GET['d']) && $_GET['d'] == "requests"){
 	 $controller->Display("Address", "AddressRequests", $address_id);
+ } 
+ if(isset($_GET['d']) && $_GET['d'] == "allowance"){
+	 $controller->Display("Allowance", "AllowanceAllowance", $address_id);
  }
  if(isset($_GET['d']) && $_GET['d'] == "names"){
 	 $controller->Display("Address", "AddressNames", $address_id);
