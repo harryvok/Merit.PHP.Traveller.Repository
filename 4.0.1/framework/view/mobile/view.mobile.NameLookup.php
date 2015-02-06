0249f2d66e8a3ea9e156a1705b93a33ab69ab5cf
phirpara@merit.com.au <phirpara@merit.com.au>
2015-02-06 15:18:34 +1100

    dsd

--- 4.0.1/framework/view/mobile/view.mobile.NameLookup.php
+++ 4.0.1/framework/view/mobile/view.mobile.NameLookup.php
@@ -6,7 +6,6 @@ if(isset($GLOBALS['result']->name_search_det->name_search) && count($GLOBALS['re
 	 $(document).ready(function(){
 	    $("#popup").popup("open");	     
 	    $("#default").page('destroy').page();
-	    $(document).scrollTop(100);
 		$('.name_row').click(function(){
 			var id = $(this).attr('id');
 			$('#pref_title').val($('#ret_'+id+'_pref_title').val());
