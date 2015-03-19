324391afad1f58e1beb413ddd049cde624d1603e
phirpara@merit.com.au <phirpara@merit.com.au>
2015-03-19 11:28:08 +1100

    search

--- 4.0.1/page.search.php
+++ 4.0.1/page.search.php
@@ -1,7 +1,9 @@
  <script type="text/javascript">
      $(document).ready(function () {
          $("#action").val("Search");
-         if ($("#search").val() != "" || $("#search").val() != null) {
+         if ($("#search").val() == "" || $("#search").val() == null) {
+         }
+         else{
              get_search();
          }
         $("#search_adv").click(function () {
@@ -44,7 +46,8 @@
             }
         });
     });
-    function get_search() {
+     function get_search() {
+         alert($("#action").val());
         Load();
         if ($("#action").val() == "Search") {
             $.ajax({
@@ -96,74 +99,6 @@
     <div id="adv_search_query" style="display:none">
     </div>
     <div id="form">
-        <script type="text/javascript">
-            $(document).ready(function () {
-                $("#submit").click(function () {
-                    $(".dateField").each(function (index, element) {
-                        if ($(element).val() == "dd/mm/yyyy") {
-                            $(element).val("");
-                        }
-                    });
-                    $(".timeField").each(function (index, element) {
-                        if ($(element).val() == "--:-- --") {
-                            $(element).val("");
-                        }
-                    });
-                    submit();
-                });
-
-                $('body').on('keyup', function (e) {
-                    if (e.which == 13) {
-                        $(".dateField").each(function (index, element) {
-                            if ($(element).val() == "dd/mm/yyyy") {
-                                $(element).val("");
-                            }
-                        });
-                        $(".timeField").each(function (index, element) {
-                            if ($(element).val() == "--:-- --") {
-                                $(element).val("");
-                            }
-                        });
-                        submit();
-                    }
-                });
-                $("input[type='reset']").click(function () {
-                    $("input[type=text],textarea,select").val("");
-                    $("#global-udfs").html("");
-                    $("#udfs").hide();
-                    $("#service").val("");
-                    $("#request").val("");
-                    $("#function").val("");
-                });
-
-                function submit() {
-
-                    Load();
-                    $.ajax({
-                        url: 'inc/ajax/ajax.RequestSearch.php',
-                        data: $("#advancedSearch").serialize(),
-                        type: 'POST',            
-                        success: function (data) {
-                            Unload();
-                            $("#searchResults").html(data);
-                            window.scrollTo(0, 0);
-                        },
-                        error: function (x, t, m) {
-                            Unload();                        
-                                alert("There was an error with the search. Please Narrow your search and try again.");                         
-                        }                       
-                    });
-                }
-
-                $("#reset").click(function () {
-                    $("#searchResults").html("");
-                });
-
-                
-
-            });
-
-        </script>
         <form id="advancedSearch">
             <input type='hidden' name='udfs_exist' id='udfs_exist' value='0' />
             <div class="float-left">
