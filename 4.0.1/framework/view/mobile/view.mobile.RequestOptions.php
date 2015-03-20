81ced3edefe9e02a866cf6aee163d85401346734
James Thomas <jthomas@merit.com.au>
2015-03-20 14:48:26 +1100

    fixes

--- 4.0.1/framework/view/mobile/view.mobile.RequestOptions.php
+++ 4.0.1/framework/view/mobile/view.mobile.RequestOptions.php
@@ -1,11 +1,12 @@
 <div data-role="collapsible" data-collapsed="false">
     <script type="text/javascript">
+
         $(document).ready(function () {
 
-            $(".notifyOfficerOption").click(function () {
+            $("#noteofficer").click(function () {
                 if (confirm("A Notification will be sent to the Inusrance Officer. Do you want to proceed?")) {
                     notifyInsuranceOfficer();
-                    $(".notifyOfficerOption").blur();
+                    location.reload();
                 }
             });
 
@@ -18,7 +19,7 @@
             <?php } elseif($GLOBALS['finalised_ind'] == "N" && $_SESSION['roleSecurity']->maint_recat == "Y" || $GLOBALS['count_only'] == "Y" && $_SESSION['roleSecurity']->maint_recat == "Y"){  ?>
             <a data-role="button" href="index.php?page=view-request&id=<?php echo $_GET['id']; ?>&d=recategoriseRequest">Recategorise Request</a><?php } ?>
             <?php if($_SESSION['roleSecurity']->maint_req_del == "Y") { ?><a data-role="button" href="index.php?page=view-request&id=<?php echo $_GET['id']; ?>&d=deleteRequest">Delete Request</a><?php } ?>
-            <?php if($_SESSION['notify_insurance'] == "Y") { ?><a data-role="button" href="#" class="notifyOfficerOption">Notify Insurance Officer</a><?php } ?>
+            <?php if($_SESSION['notify_insurance'] == "Y") { ?><a data-role="button" id="noteofficer" href="#" class="notifyOfficerOption">Notify Insurance Officer</a><?php } ?>
     </p>
 </div>
 
